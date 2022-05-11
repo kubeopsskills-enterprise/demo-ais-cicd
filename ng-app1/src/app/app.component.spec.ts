da2d9e409f6ab48c4d2c909ce3d1eb31a65b0955
wasuwat-kubeops <wasuwat.p@kubeops.guru>
2022-05-12 01:47:54 +0700

    Update: apps

--- ng-app1/src/app/app.component.spec.ts
+++ ng-app1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'man-ng'`, () => {
+  it(`should have as title 'ng-app1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('man-ng');
+    expect(app.title).toEqual('ng-app1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('man-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-app1 app is running!');
   });
 });
--- ng-app1/src/app/app.component.ts
+++ ng-app1/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'man-ng';
+  title = 'ng-app1';
   secret = environment.someSecret;
 }
